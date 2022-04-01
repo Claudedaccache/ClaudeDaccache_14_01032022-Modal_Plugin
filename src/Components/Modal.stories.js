e0855fb05975b7b930281d1a0dfd41ef4468cded
Claude Daccache <claude.daccache@outlook.com>
2022-04-01 11:22:45 +0200

    modified the stories.js file

--- src/Components/Modal.stories.js
+++ src/Components/Modal.stories.js
@@ -5,6 +5,65 @@ import Modal from "./Modal";
 export default {
   title: "Example/Modal",
   component: Modal,
+    argTypes: {
+    ModalStatus: {
+      name: "ModalStatus",
+      type: { name: "string", required: true },
+      description: "Declare Modal Status",
+      control: {
+        type: "boolean",
+      },
+    },
+    setModalStatus: {
+      name: "setModalStatus",
+      type: { name: "string", required: true },
+      description: "Modify Modal Status",
+      control: {
+        type: null,
+      },
+      action: "setModalStatus"
+    },
+    ModalContainerStyle: {
+      name: "ModalContainerStyle",
+      type: { name: "string" },
+      description: "Style Modal container",
+      control: {
+        type: "object",
+      },
+    },
+    ModalBodyStyle: {
+      name: "ModalBodyStyle",
+      type: { name: "string" },
+      description: "Style Modal Body",
+      control: {
+        type: "object",
+      },
+    },
+    ModalInnerStyle: {
+      name: "ModalInnerStyle",
+      type: { name: "string" },
+      description: "Style Modal Children",
+      control: {
+        type: "object",
+      },
+    },
+    ModalCloseIconStyle: {
+      name: "ModalCloseIconStyle",
+      type: { name: "string" },
+      description: "Style Modal Close Icon",
+      control: {
+        type: "object",
+      },
+    },
+    ModalChildren: {
+      name: "ModalChildren",
+      type: { name: "string" },
+      description: "Modal children",
+      control: {
+        type: "text",
+      },
+    },
+  },
 };
 
 const Template = (args) => <Modal {...args} />;
